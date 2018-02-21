5b822af9c8bbc1c41de27b8d9e02d15d03cab894
Mike Schwager <mike@schwager.com>
2018-02-21 17:36:55 -0600

    Updated copyright.

--- examples/example_base_classes.py
+++ examples/example_base_classes.py
@@ -1,4 +1,4 @@
-#    Copyright 2017 Michael Schwager
+#    Copyright 2017, 2018 Michael Schwager
 #    Copyright 2016, 2015, 2014, 2013, 2012 Pavel Kostelnik
 
 #   Licensed under the Apache License, Version 2.0 (the "License");
