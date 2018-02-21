852e5305db1fb864cae82fc13a93a16ae8d71abe
Mike Schwager <mike@schwager.com>
2018-02-21 17:38:34 -0600

    Gave the setup a good once-over for 0.5 release.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='kivydnd',
-      version='0.2',
-      description='Kivy Drag-n-Drop for Widgets',
-      url='https://github.com/GreyGnome/KivyDnD',
-      author='GreyGnome',
-      license='Apache License 2.0',
-      packages=['kivydnd'])
+setup(
+    name='kivydnd',
+    version='0.5',
+    description='Kivy Drag-n-Drop for Widgets',
+    url='https://github.com/GreyGnome/KivyDnD',
+    author='GreyGnome',
+    author_email='mschwage@gmail.com',
+    license='Apache License 2.0',
+    packages=find_packages('kivydnd','examples'),
+    package_data={
+        '': ['*.md'],
+        'examples': ['*'],
+    },
+)
