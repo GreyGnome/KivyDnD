bafd5695d662a89b7b9eb3600157849cf3bf854e
Mike Schwager <mike@schwager.com>
2018-04-09 08:09:44 -0500

    Fixed the setup.py so it works with pip install. Tested on Windows 8.

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+with open(path.join('.', 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='kivydnd',
-    version='0.5',
+    version='0.5.0',
     description='Kivy Drag-n-Drop for Widgets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/GreyGnome/KivyDnD',
     author='GreyGnome',
     author_email='mschwage@gmail.com',
     license='Apache License 2.0',
-    packages=find_packages('kivydnd','examples'),
-    package_data={
-        '': ['*.md'],
-        'examples': ['*'],
-    },
+    keywords='kivy drag-n-drop',
+    packages=find_packages(exclude=[]),
+    data_files=[('share/kivydnd-examples',
+        [
+            'examples/dndexample1.py',
+            'examples/dndexample2.py',
+            'examples/dndexample3.py',
+            'examples/dndexample_copy_draggable.py',
+            'examples/dndexample_drop_groups.py',
+            'examples/dndexample_relative_layout.py',
+            'examples/example_base_classes.py',
+            'examples/example_base_classes.pyc',
+        ]
+    )],
 )
+#   packages=['kivydnd'],
